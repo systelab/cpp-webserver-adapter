a0988c91f8df03dfe3e12d278a51f3ab811a24e1
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-03-23 18:40:40 +0100

    Comparator for the new security configuration object

---
+++ WebServerAdapterTestUtilities/Comparators/SecurityConfigurationComparator.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "WebServerAdapterInterface/Model/SecurityConfiguration.h"
+
+#include "TestUtilitiesInterface/EntityComparator.h"
+
+
+using namespace testing;
+
+namespace systelab { namespace test_utility {
+
+	template <>
+	testing::AssertionResult EntityComparator::operator() (const systelab::web_server::SecurityConfiguration& expected, const systelab::web_server::SecurityConfiguration& actual) const
+	{
+		COMPARATOR_ASSERT_EQUAL(expected, actual, isHTTPSEnabled());
+		COMPARATOR_ASSERT_EQUAL(expected, actual, getServerCertificate());
+		COMPARATOR_ASSERT_EQUAL(expected, actual, getServerPrivateKey());
+		COMPARATOR_ASSERT_EQUAL(expected, actual, getServerDHParam());
+
+		COMPARATOR_ASSERT_EQUAL(expected, actual, isMutualSSLEnabled());
+		COMPARATOR_ASSERT_EQUAL(expected, actual, getClientCertificate());
+
+		return AssertionSuccess();
+	}
+
+}}
