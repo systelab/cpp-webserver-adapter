c6cc5a0215af89a6c362663e681c080dd6e00b38
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-03-23 18:39:24 +0100

    Added interface for server factory

---
+++ WebServerAdapterInterface/IServerFactory.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace systelab { namespace web_server {
+
+	class Configuration;
+	class IServer;
+
+	class IServerFactory
+	{
+	public:
+		virtual ~IServerFactory() = default;
+
+		virtual std::unique_ptr<IServer> buildServer(const Configuration&) const = 0;
+	};
+
+}}
