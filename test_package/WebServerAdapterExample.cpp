90f1fcd6e59588c42f24bfcddcaea41d07a8586e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-08-02 00:43:11 +0200

    Added test package for WebServer adapter

---
+++ test_package/WebServerAdapterExample.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+
+#include "WebServerAdapterTestUtilities/Mocks/MockServer.h"
+
+
+int main(int argc, char *argv[])
+{
+    systelab::web_server::test_utility::MockServer server;
+    std::cout << "Web server adapter test utilities work as expected" << std::endl;
+
+    return 0;
+}
