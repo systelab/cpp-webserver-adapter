ccce908327b700a7e189e1a594e4765ad00ce58d
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-09-30 12:01:29 +0200

    Export sources for web server adapter interface

--- WebServerAdapterInterface/conanfile.py
+++ WebServerAdapterInterface/conanfile.py
@@ -11,6 +11,7 @@ class WebServerAdapterInterfaceConan(ConanFile):
     license = "MIT"
     generators = "cmake_find_package"
     # No settings/options are necessary, this is header only
+    exports_sources = "*"
 
     def package(self):
         self.copy("*.h", dst="include/WebServerAdapterInterface")
